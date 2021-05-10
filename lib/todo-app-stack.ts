67700d74fcb141aaecc4546d25550baf1513e926
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2021-05-10 19:20:15 +0100

    adding dynamoDB

--- lib/todo-app-stack.ts
+++ lib/todo-app-stack.ts
@@ -3,10 +3,14 @@ import * as lambda from '@aws-cdk/aws-lambda';
 import * as apiGateway from '@aws-cdk/aws-apigateway'
 import * as s3 from '@aws-cdk/aws-s3'
 import * as s3Notifications from '@aws-cdk/aws-s3-notifications'
+import * as s3Deployment from '@aws-cdk/aws-s3-deployment'
+import { TodoBackend } from './todo-backend'
 
 export class TodoAppStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const todoBackend = new TodoBackend(this, "TodoBackend");
 
     const helloLambda = new lambda.Function(this, 'Hellolambda', {
       code: lambda.Code.fromAsset('lambda'),
@@ -29,5 +33,9 @@ export class TodoAppStack extends cdk.Stack {
       s3.EventType.OBJECT_CREATED,
       new s3Notifications.LambdaDestination(helloLambda)
     )
+    new s3Deployment.BucketDeployment(this, "DeployLogo", {
+      destinationBucket: logoBucket,
+      sources: [s3Deployment.Source.asset("./assets")]
+    })
   }
 }
